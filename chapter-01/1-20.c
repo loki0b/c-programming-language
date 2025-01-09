efe392c6074ca9791c14be6aa0bf73b58d080657
loki0b <social@henriquegarcialeite.com>
2025-01-09 08:57:46 -0300

    update 1-20

--- chapter-01/1-20.c
+++ chapter-01/1-20.c
@@ -1,22 +1,31 @@
+/* 
+ * Write a program detab that replaces tabs in the input with the proper number of blanks to space to the next tab stop.
+ * Assume a fixed set of tab stops, say every n columns. Should n be a variable or a symbolic parameter?
+ */
 #include <stdio.h>
 
-#define LIMIT 1000
+#define BUFFER 1024
+#define TABSTOP 8
+#define TAB     '\t'
+#define BLANK   ' '
+#define NEWLINE '\n'
 
 int main() {
-    int c;
-    char line[LIMIT];
+    int c, i;
+    char line[BUFFER];
 
-
+    i = 0;
     while ((c = getchar ()) !=  EOF) {
-        if (c == '\t') {
-            int i;
-
-            i = 0;
-            while (i++ < 2)
-                putchar(' ');
+        if (c == TAB) {
+            while (i++ < TABSTOP)
+                putchar('#');
         }
-        else
+        else {
             putchar(c);
+            ++i;
+        }
+        if (i >= TABSTOP || c == NEWLINE)
+            i = 0;
     }
 
     return 0;
